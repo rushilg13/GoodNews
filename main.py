aca554219ce946a35602dd9e2fb5b7bdb5de3faa
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2022-01-14 12:40:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# Import Modules
+from fastapi import FastAPI
+import pickle
+from starlette.responses import Response
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import requests
+
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('tranform.pkl', 'rb'))
+
+middleware = [ Middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])]
+
+app = FastAPI(middleware=middleware)
+
+@app.get('/{category}/{location}')
+def home(category:str, location:str):
+    Response = news(category, location)
+    return Response["articles"]
+
+@app.post('/')
+def news(category, location):
+    URL = "https://saurav.tech/NewsAPI/top-headlines/category/{0}/{1}.json".format(category, location)
+    response = requests.get(URL)
+    # response = requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=7bf43f1b86894a14990f6ddeb0b32dac")
+    response = response.json()
+    for article in response["articles"]:
+        headline = article["title"]
+        positive_headline = predict_news(headline)
+        if int(positive_headline) == 1:
+            article["sentiment"] = "positive"
+        else:
+            article["sentiment"] = "negative"
+    print("Prediction done")
+    return response
+
+def predict_news(data:str):
+    msg = [data]
+    vect = cv.transform(msg).toarray()
+    my_prediction = clf.predict(vect)
+    return my_prediction
