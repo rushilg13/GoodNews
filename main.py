be6c747d4e5ffc5a6421e39c0c30b68f5dea2e94
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2022-01-20 17:48:08 +0530

    downgraded

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import requests
 from fastapi.responses import FileResponse
-import pymongo  
-import json
-from bson import json_util
-from datetime import datetime
 
 # Load Pickle files
 filename = 'nlp_model.pkl'
@@ -23,37 +19,16 @@ middleware = [ Middleware(CORSMiddleware, allow_origins=['*'], allow_credentials
 # Set up app
 app = FastAPI(middleware=middleware)
 
-# connect to MongoDB
-myclient = pymongo.MongoClient("mongodb+srv://VIT_Admin:pizza@vitdiaries.tpuku.mongodb.net/GoodNews?retryWrites=true&w=majority")
-mydb = myclient["GoodNews"]
-
 # Routes
 @app.get('/{category}/{location}')
-async def home(category:str, location:str):
-    news(category, location)
-    data = []
-    if category == "all" and location == "all":
-        cats = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
-        locs = ["in", "us", "gb", "fr", "au", "ru"]
-        for cat in (cats):
-            for loc in (locs):
-                col_name = "{0}_{1}".format(cat, loc)
-                mycol = mydb[col_name]
-                Response = mycol.find({}).sort('publishedAt',pymongo.DESCENDING)
-                Response = parse_json(Response)
-                data = data + Response
-        return data
-    else:
-        col_name = "{0}_{1}".format(category, location)
-        mycol = mydb[col_name]
-        Response = mycol.find({}).sort('publishedAt',pymongo.DESCENDING)
-        Response = parse_json(Response) 
-        return Response
+def home(category:str, location:str):
+    Response = news(category, location)
+    return Response
 
-# @app.post('/')
 def news(category, location):
     # For getting all the news articles
     if (category == "all" and location == "all"):
+        counter = 0
         all_articles = []
         cats = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
         locs = ["in", "us", "gb", "fr", "au", "ru"]
@@ -68,23 +43,8 @@ def news(category, location):
                     if int(positive_headline) == 1:
                         all_articles.append(article)
                         article["sentiment"] = "positive"
-                        d_t = article["publishedAt"].split("T")
-                        d = d_t[0].split("-")
-                        year, month, date = d[0], d[1], d[2]
-                        t = d_t[1].split(":")
-                        hour, minute, sec = t[0], t[1], t[2]
-                        sec = sec[:1]
-                        pub_at = datetime(int(year), int(month), int(date), int(hour), int(minute), int(sec))
-                        article["publishedAt"] = pub_at
-                        col_name = "{0}_{1}".format(cat, loc)
-                        mycol = mydb[col_name]
-                        Response = mycol.find({"title" : headline})
-                        Response = parse_json(Response)
-                        if Response == []:
-                            x = mycol.insert_one(article)
-                            print("Document inserted with id: ", x.inserted_id)
-                        else:
-                            print("Record already exists")
+                        article["article_ID"] = counter
+                        counter += 1
                     else:
                         article["sentiment"] = "negative"
                         article.clear()
@@ -93,6 +53,7 @@ def news(category, location):
     
     # For particular category and location
     else:
+        counter = 0
         all_articles = []
         URL = "https://saurav.tech/NewsAPI/top-headlines/category/{0}/{1}.json".format(category, location)
         response = requests.get(URL)
@@ -104,27 +65,13 @@ def news(category, location):
             if int(positive_headline) == 1:
                 all_articles.append(article)
                 article["sentiment"] = "positive"
-                d_t = article["publishedAt"].split("T")
-                d = d_t[0].split("-")
-                year, month, date = d[0], d[1], d[2]
-                t = d_t[1].split(":")
-                hour, minute, sec = t[0], t[1], t[2]
-                sec = sec[:1]
-                pub_at = datetime(int(year), int(month), int(date), int(hour), int(minute), int(sec))
-                article["publishedAt"] = pub_at
-                col_name = "{0}_{1}".format(category, location)
-                mycol = mydb[col_name]
-                Response = mycol.find({"title" : headline})
-                Response = parse_json(Response)
-                if Response == []:
-                    x = mycol.insert_one(article)
-                    print("Document inserted with id: ", x.inserted_id)
-                else:
-                    print("Record already exists")
+                article["article_ID"] = counter
+                counter += 1
             else:
                 article["sentiment"] = "negative"
                 article.clear()
     print("Prediction done")
+    return all_articles
 
 # Prediction Function
 def predict_news(data:str):
@@ -137,8 +84,3 @@ def predict_news(data:str):
 @app.get("/records")
 def records():
     return FileResponse("a.txt")
-
-def parse_json(data):
-    return json.loads(json_util.dumps(data))
-
-# Insert if not exist   
